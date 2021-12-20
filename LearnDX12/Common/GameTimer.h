f51f1f5fababd780099c696ea80e3767f8e25b2a
shishifeng <1677152168@qq.com>
2021-12-20 20:14:47 +0800

    自己写一遍Comman函数，加深理解

--- LearnDX12/Common/GameTimer.h
+++ LearnDX12/Common/GameTimer.h
@@ -1,34 +1,28 @@
-//***************************************************************************************
-// GameTimer.h by Frank Luna (C) 2011 All Rights Reserved.
-//***************************************************************************************
-
-#ifndef GAMETIMER_H
-#define GAMETIMER_H
+﻿#pragma once
 
 class GameTimer
 {
 public:
-	GameTimer();
+    GameTimer();
 
-	float TotalTime()const; // in seconds
-	float DeltaTime()const; // in seconds
+    float TotalTime() const;
+    float DeltaTime() const;
 
-	void Reset(); // Call before message loop.
-	void Start(); // Call when unpaused.
-	void Stop();  // Call when paused.
-	void Tick();  // Call every frame.
+    void Reset();   //重置
+    void Start();   //开始计时
+    void Stop();    //暂停计时
+    void Tick();    //每帧调用.
 
 private:
-	double mSecondsPerCount;
-	double mDeltaTime;
+    double mSecondsPerCount;
+    double mDeltaTime;
 
-	__int64 mBaseTime;
-	__int64 mPausedTime;
-	__int64 mStopTime;
-	__int64 mPrevTime;
-	__int64 mCurrTime;
+    // __int64 是Windows下的64位
+    __int64 mBaseTime;
+    __int64 mPausedTime;
+    __int64 mStopTime;
+    __int64 mPrevTime;
+    __int64 mCurrTime;
 
-	bool mStopped;
+    bool mStopped;
 };
-
-#endif // GAMETIMER_H
